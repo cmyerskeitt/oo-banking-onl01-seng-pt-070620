47a2041ad30babaab1d58b4021785508635e6e67
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-08-04 03:41:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,9 @@
 class Transfer
-  attr_accessor:bank_account, :name
+  attr_accessor:bank_account, :name, :receiver, :amount 
   
   def initialize(sender, receiver, amount )
     @sender= sender
     @receiver= receiver 
-    
+    @amount = amount 
   end 
 end
