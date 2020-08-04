8a90c8e4777eed62a63436aea494e467da032cca
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-08-04 03:49:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,8 @@ class Transfer
   end 
   
   def execute_transaction
-    @sender.deposit( @amount * -1 ) 
+    if @sender.deposit( @amount * -1 ) 
       @receiver.deposit( @amount )
+    end 
   end 
 end
