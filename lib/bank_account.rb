d50b39a1e21cc0d6c7bc076736f05eac6fb27f31
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-08-04 03:33:47 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -16,8 +16,8 @@ class BankAccount
   end   
   
   def valid?
-    if @status == "open" && @balance > 0 
-      true 
+   if  @status == "open" && @balance > 0 
+     true
     else 
       false
     end 
